24351652cf75ef6c306a116d1c895d969983a744
Tim Abraldes <tabralde@amazon.com>
2020-04-18 04:51:58 +0000

    Fisher

--- fish/config.fish
+++ fish/config.fish
@@ -36,6 +36,12 @@ abbr --add gs git status
 
 set fish_term24bit 1
 
+if not functions -q fisher
+  set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+  curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+  fish -c fisher
+end
+
 if command -v tmux; and test -z $TMUX
   exec tmux new-session -A -s mysession
 end
