eeb56b66c805da7841764845ff804cf8492c00a4
Tim Falletta Abraldes <github.rjww2@slmail.me>
2024-10-02 13:16:11 -0700

    Only check in some fish config files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,8 @@
 set fish_term24bit 1
 
 set -g fish_key_bindings fish_vi_key_bindings
+
+if not set --query INSTALLING_FISHER && not test -e "~/.config/fish/functions/fisher.fish"
+	set -x INSTALLING_FISHER 1
+	curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update
+end
