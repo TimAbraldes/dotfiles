393bad0638fbe2092bc1a8ae6b0231338c764a96
Tim Falletta Abraldes <github.rjww2@slmail.me>
2024-10-02 13:33:09 -0700

    Change the way we test for fisher

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ set fish_term24bit 1
 
 set -g fish_key_bindings fish_vi_key_bindings
 
-if not set --query INSTALLING_FISHER && not test -e "~/.config/fish/functions/fisher.fish"
+if not set --query INSTALLING_FISHER && not type --quiet "fisher"
 	set -x INSTALLING_FISHER 1
 	curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update
 end
