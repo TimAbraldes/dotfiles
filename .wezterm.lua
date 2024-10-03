ec225f14bd861a3132e1409dedb3e6fb95ee4159
Tim Falletta Abraldes <github.rjww2@slmail.me>
2024-10-03 15:50:44 -0700

    Rainbow bg for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,5 +7,12 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 config.term = 'wezterm'
 
+config.window_background_gradient = {
+  colors = { '#300000', '#303000', '#003000', '#003030', '#000030' },
+
+  -- Specifies a Linear gradient starting in the top left corner.
+  orientation = { Linear = { angle = -45.0 } },
+}
+
 -- and finally, return the configuration to wezterm
 return config
