9801a58ac4dc4a47f68da5d35e2fec75839295a5
Tim Falletta Abraldes <github.rjww2@slmail.me>
2024-10-04 08:58:02 -0700

    Change angle of rainbow in wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,9 +9,7 @@ config.term = 'wezterm'
 
 config.window_background_gradient = {
   colors = { '#300000', '#303000', '#003000', '#003030', '#000030', '#300030' },
-
-  -- Specifies a Linear gradient starting in the top left corner.
-  orientation = { Linear = { angle = -45.0 } },
+  orientation = { Linear = { angle = -20.0 } },
 }
 
 -- and finally, return the configuration to wezterm
