df573dd8f61a2bfac9dc9eb2f2195c2b9abf8566
Tim Falletta Abraldes <github.rjww2@slmail.me>
2024-10-04 09:12:04 -0700

    Add logging around fisher installation

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,6 +13,8 @@ set fish_term24bit 1
 set -g fish_key_bindings fish_vi_key_bindings
 
 if not set --query INSTALLING_FISHER && not type --quiet "fisher"
-	set -x INSTALLING_FISHER 1
+	set --export INSTALLING_FISHER 1
+	echo "START Installing fisher"
 	curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update
+	echo "DONE Installing fisher"
 end
