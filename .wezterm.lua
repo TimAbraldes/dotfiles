93c40f5eaa02590f500c1e6478b54bfe2e888d30
Tim Falletta Abraldes <github.rjww2@slmail.me>
2024-10-04 09:25:23 -0700

    adjust_window_size_when_changing_font_size = false

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 config.term = 'wezterm'
-
+config.adjust_window_size_when_changing_font_size = false
 config.window_background_gradient = {
   colors = { '#300000', '#303000', '#003000', '#003030', '#000030', '#300030' },
   orientation = { Linear = { angle = -20.0 } },
