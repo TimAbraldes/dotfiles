3e91b254acaa9137ad79d8f1a670095ad79dc82d
Tim Falletta Abraldes <github.rjww2@slmail.me>
2024-10-04 13:49:44 -0700

    Refactor fish config, fisher update on every launch

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,7 @@
 # Install wezterm terminfo if not already installed
 if not toe -a | grep wezterm &> /dev/null
-	set --export INSTALLING_WEZTERM_TERMINFO 1
-	set --local tempfile $(mktemp)
 	echo "START Installing wezterm terminfo file"
+	set --local tempfile $(mktemp)
 	curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo
 	tic -x -o ~/.terminfo $tempfile
 	rm $tempfile
@@ -13,15 +12,26 @@ end
 set fish_term24bit 1
 
 # Install fisher if not already installed
-if not set --query INSTALLING_FISHER && not type --quiet "fisher"
-	set --export INSTALLING_FISHER 1
+if not set --query FISHER_INSTALL && not type --quiet "fisher"
 	echo "START Installing fisher"
+	set --export FISHER_INSTALL 1
+
 	curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
+
 	echo "DONE Installing fisher"
+	set --erase FISHER_INSTALL
 end
 
 # Install / update any fisher plugins
-fisher update
+if not set --query FISHER_UPDATE
+	echo "START fisher update"
+	set --export FISHER_UPDATE 1
+
+	fisher update
+
+	set --erase FISHER_UPDATE
+	echo "DONE fisher update"
+end
 
 # Use this area for machine-agnostic configuration (checked in to main branch of dotfiles repo)
 set -g fish_key_bindings fish_vi_key_bindings
