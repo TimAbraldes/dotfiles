ce90e4cf11e7eda89dcd3c704aeac8aaf2e1b18d
Tim Falletta Abraldes <github.rjww2@slmail.me>
2024-10-04 13:42:27 -0700

    Fish config updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
+# Install wezterm terminfo if not already installed
 if not toe -a | grep wezterm &> /dev/null
 	set --export INSTALLING_WEZTERM_TERMINFO 1
 	set --local tempfile $(mktemp)
@@ -8,13 +9,21 @@ if not toe -a | grep wezterm &> /dev/null
 	echo "DONE Installing wezterm terminfo file"
 end
 
+# Now that we have terminfo installed, make sure that fish knows we have 24-bit color support
 set fish_term24bit 1
 
-set -g fish_key_bindings fish_vi_key_bindings
-
+# Install fisher if not already installed
 if not set --query INSTALLING_FISHER && not type --quiet "fisher"
 	set --export INSTALLING_FISHER 1
 	echo "START Installing fisher"
-	curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update
+	curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
 	echo "DONE Installing fisher"
 end
+
+# Install / update any fisher plugins
+fisher update
+
+# Use this area for machine-agnostic configuration (checked in to main branch of dotfiles repo)
+set -g fish_key_bindings fish_vi_key_bindings
+
+# Use this area for machine-specific configuration (checked in to dotfiles repo in separate branches)
