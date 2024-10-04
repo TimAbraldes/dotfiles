09cfc5813d0a9b09169037a284df6c61408e6e7f
Tim Falletta Abraldes <github.rjww2@slmail.me>
2024-10-04 09:11:44 -0700

    On fish startup, install wezterm terminfo

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,13 @@
+if not toe -a | grep wezterm &> /dev/null
+	set --export INSTALLING_WEZTERM_TERMINFO 1
+	set --local tempfile $(mktemp)
+	echo "START Installing wezterm terminfo file"
+	curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo
+	tic -x -o ~/.terminfo $tempfile
+	rm $tempfile
+	echo "DONE Installing wezterm terminfo file"
+end
+
 set fish_term24bit 1
 
 set -g fish_key_bindings fish_vi_key_bindings
