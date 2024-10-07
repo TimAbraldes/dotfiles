9f4879ec42e7d0b8d5c0393ceadfd1f6b6a27a5a
Tim Falletta Abraldes <github.rjww2@slmail.me>
2024-10-07 11:21:40 -0700

    config.fish: Guard at top level. Change logging. nvm install

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,11 @@
+# Some commands that we'll run (e.g. fisher) will invoke fish, causing this file to be processed recursively
+# Guard against that situation with an environment variable
+if set --query ALREADY_PROCESSING_FISH_CONFIG
+	exit
+end
+
+set --export ALREADY_PROCESSING_FISH_CONFIG 1
+
 # Install wezterm terminfo if not already installed
 if not toe -a | grep wezterm &> /dev/null
 	echo "START Installing wezterm terminfo file"
@@ -6,30 +14,26 @@ if not toe -a | grep wezterm &> /dev/null
 	tic -x -o ~/.terminfo $tempfile
 	rm $tempfile
 	echo "DONE Installing wezterm terminfo file"
+else
+	echo "SKIP Installing wezterm terminfo file"
 end
 
 # Now that we have terminfo installed, make sure that fish knows we have 24-bit color support
 set fish_term24bit 1
 
 # Install fisher if not already installed
-if not set --query FISHER_INSTALL && not type --quiet "fisher"
+if not type --quiet "fisher"
 	echo "START Installing fisher"
-	set --export FISHER_INSTALL 1
-
 	curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
-
 	echo "DONE Installing fisher"
-	set --erase FISHER_INSTALL
+else
+	echo "SKIP Installing fisher"
 end
 
-# Install / update any fisher plugins
-if not set --query FISHER_UPDATE
+# Install / update any fisher plugins, but not if we're starting up inside TMUX
+if not set --query TMUX; and not set --query FISHER_UPDATE
 	echo "START fisher update"
-	set --export FISHER_UPDATE 1
-
 	fisher update
-
-	set --erase FISHER_UPDATE
 	echo "DONE fisher update"
 end
 
@@ -37,3 +41,6 @@ end
 set -g fish_key_bindings fish_vi_key_bindings
 
 # Use this area for machine-specific configuration (checked in to dotfiles repo in separate branches)
+nvm install latest
+
+set --erase ALREADY_PROCESSING_FISH_CONFIG 1
