508fac4a423ed5e52ac59294644b6b129e0ef4cd
Tim Falletta Abraldes <github.rjww2@slmail.me>
2024-10-08 11:51:55 -0700

    Fish config cleanup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -27,9 +27,6 @@ else
 	echo "SKIP Installing wezterm terminfo file"
 end
 
-# Now that we have terminfo installed, make sure that fish knows we have 24-bit color support
-set fish_term24bit 1
-
 # Install fisher if not already installed
 if not type --quiet "fisher"
 	echo "START Installing fisher"
@@ -53,6 +50,7 @@ end
 
 # Use this area for machine-agnostic configuration (checked in to main branch of dotfiles repo)
 set -g fish_key_bindings fish_vi_key_bindings
+set fish_term24bit 1
 
 # Use this area for machine-specific configuration (checked in to dotfiles repo in separate branches)
 
