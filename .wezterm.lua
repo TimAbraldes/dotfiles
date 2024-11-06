84e8d6ca5148f493411576e65e3e62e694b98238
Tim Falletta Abraldes <github.rjww2@slmail.me>
2024-11-06 10:07:32 -0800

    Increase default font size in wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,6 +11,7 @@ config.window_background_gradient = {
   colors = { '#000000', '#300000', '#300F00', '#300F00', '#303000', '#303000', '#003000', '#003030', '#003030', '#000030', '#0F0030', '#0F0030', '#300030', '#000000' },
   orientation = { Linear = { angle = -20.0 } },
 }
+config.font_size = 14.0
 
 -- and finally, return the configuration to wezterm
 return config
