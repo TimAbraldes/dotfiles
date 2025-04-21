9ebc9c264b845e74298bcc4362f37ccfa82a20a8
Tim Falletta Abraldes <github.rjww2@slmail.me>
2025-04-21 11:50:01 -0700

    Disable noice.nvim

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,15 +1,18 @@
 return {
-  "folke/flash.nvim",
-  opts = {
-    modes = {
-      char = {
-        -- disable f F t T overrides. Preserve the original behavior for those keys
-        enabled = false,
+  { "folke/noice.nvim", enabled = false },
+  {
+    "folke/flash.nvim",
+    opts = {
+      modes = {
+        char = {
+          -- disable f F t T overrides. Preserve the original behavior for those keys
+          enabled = false,
+        },
       },
     },
-  },
-  keys = {
-    -- disable s override. Preserve the original behavior for that key
-    { "s", mode = { "n", "x", "o" }, false },
-  },
+    keys = {
+      -- disable s override. Preserve the original behavior for that key
+      { "s", mode = { "n", "x", "o" }, false },
+    },
+  }
 }
